c64bedf8f3a36c62fc1797ae20c7e7f88a76d400
Benjamin Atkin <ben@benatkin.com>
2014-05-07 20:23:30 -0700

    don't require() the current file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp')
 var through2 = require('through2')
+var path = require('path')
 
 function replaceProcessEnv() {
   return through2.obj(function(file, encoding, done) {
     var original = file.contents.toString('utf8');
-    var replaced = original.replace(/"production" !== process.env.NODE_ENV/g, "require('./React').__DEV__ === true")
+    var reactExpression = path.basename(file.path) === 'React.js' ? 'React' : "require('./React')";
+    var replaced = original.replace(/"production" !== process.env.NODE_ENV/g, reactExpression + ".__DEV__ === true")
     file.contents = new Buffer(replaced);
     this.push(file);
     done();
